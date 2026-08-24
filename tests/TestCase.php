3d36b553184da993cbef6de57afea24f6f24d2c1
Skyler Blue Spillers <92972770+skylerblue333@users.noreply.github.com>
2026-08-24 02:20:28 -0500

    test: bootstrap Laravel application in feature tests

--- tests/TestCase.php
+++ tests/TestCase.php
@@ -4,8 +4,17 @@ declare(strict_types=1);
 
 namespace Tests;
 
+use Illuminate\Foundation\Application;
 use Illuminate\Foundation\Testing\TestCase as BaseTestCase;
 
 abstract class TestCase extends BaseTestCase
 {
+    public function createApplication(): Application
+    {
+        /** @var Application $app */
+        $app = require __DIR__.'/../bootstrap/app.php';
+        $app->make(\Illuminate\Contracts\Console\Kernel::class)->bootstrap();
+
+        return $app;
+    }
 }
