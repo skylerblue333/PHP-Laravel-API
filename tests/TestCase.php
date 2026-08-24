c2ce589e44f3f334f3550bdaeefca382a7f1b832
Skyler Blue Spillers <92972770+skylerblue333@users.noreply.github.com>
2026-08-24 02:18:54 -0500

    test: use Laravel 12 application test bootstrap

--- tests/TestCase.php
+++ tests/TestCase.php
@@ -4,16 +4,8 @@ declare(strict_types=1);
 
 namespace Tests;
 
-use Illuminate\Foundation\Application;
 use Illuminate\Foundation\Testing\TestCase as BaseTestCase;
 
 abstract class TestCase extends BaseTestCase
 {
-    public function createApplication(): Application
-    {
-        /** @var Application $app */
-        $app = require __DIR__.'/../bootstrap/app.php';
-
-        return $app;
-    }
 }
